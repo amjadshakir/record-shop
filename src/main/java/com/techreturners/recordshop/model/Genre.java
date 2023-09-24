a2150e58b70e0673d987d91fcb3bf151c73a0072
Swapna V <vswapna3202@gmail.com>
2023-09-24 20:45:30 +0100

    feat: added model and application-dev.properties

---
+++ src/main/java/com/techreturners/recordshop/model/Genre.java
@@ -0,0 +1,13 @@
+package com.techreturners.recordshop.model;
+
+public enum Genre {
+    Rock,
+    Jazz,
+    Country,
+    HipHop,
+    Reggae,
+    Funk,
+    Pop,
+    Metal,
+    Instrumental
+}
---
+++ src/main/java/com/techreturners/recordshop/model/Record.java
@@ -0,0 +1,31 @@
+package com.techreturners.recordshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Record {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    Long id;
+
+    @Column
+    String albumName;
+
+    @Column
+    String artist;
+
+    @Column
+    Integer releaseYear;
+
+    @Column
+    Long stock;
+
+    @Column
+    Genre genre;
+}
