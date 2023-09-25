956f0ea118bc819989ea59263623cb90600c14f0
Swapna V <vswapna3202@gmail.com>
2023-09-25 14:40:54 +0100

    feat: Added testcase for Controller and code for executing testcase

--- src/main/java/com/techreturners/recordshop/service/RecordManagerServiceImpl.java
+++ src/main/java/com/techreturners/recordshop/service/RecordManagerServiceImpl.java
@@ -1,4 +1,32 @@
 package com.techreturners.recordshop.service;
 
+import com.techreturners.recordshop.exception.RecordAlreadyExistsException;
+import com.techreturners.recordshop.model.MusicRecord;
+import com.techreturners.recordshop.repository.RecordManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class RecordManagerServiceImpl implements RecordManagerService {
+
+    @Autowired
+    RecordManagerRepository musicRecordManagerRepository;
+
+    @Override
+    public MusicRecord insertMusicRecord(MusicRecord musicRecord) {
+        String albumName = musicRecord.getAlbumName();
+        if (albumName != null) {
+            Optional<MusicRecord> bookOptional =
+                    musicRecordManagerRepository.findByAlbumName(albumName);
+            if (bookOptional.isPresent()) {
+                throw new RecordAlreadyExistsException("Album with name: " +
+                        musicRecord.getAlbumName() +
+                        " already exists and cannot be inserted again");
+            }
+        }
+        return musicRecordManagerRepository.save(musicRecord);
+    }
+
 }
