155f8642cbb22944fd3943835a66974e9619ba0e
Swapna V <vswapna3202@gmail.com>
2023-09-25 16:27:14 +0100

    feat: Added Exception RecordAlreadyExists

--- src/main/java/com/techreturners/recordshop/service/RecordManagerServiceImpl.java
+++ src/main/java/com/techreturners/recordshop/service/RecordManagerServiceImpl.java
@@ -25,8 +25,43 @@ public class RecordManagerServiceImpl implements RecordManagerService {
                         musicRecord.getAlbumName() +
                         " already exists and cannot be inserted again");
             }
+            validateInputValues(musicRecord);
         }
         return musicRecordManagerRepository.save(musicRecord);
     }
 
+    private boolean validateInputValues(MusicRecord musicRecord){
+        Integer releaseYear = musicRecord.getReleaseYear();
+        Long stock = musicRecord.getStock();
+        if (validateReleaseYear(releaseYear) && validateStock(stock))
+            return true;
+        return false;
+    }
+
+    private static boolean validateStock(Long stock) {
+        if (stock != null){
+            if (stock instanceof Long &&
+                stock >= 0) {
+                return true;
+            }else{
+                throw new NumberFormatException("Invalid input for Stock. "+
+                        "Please enter valid positive integer value for stock");
+            }
+        }
+        return false;
+    }
+
+    private static boolean validateReleaseYear(Integer releaseYear) {
+        if (releaseYear != null) {
+            if (releaseYear instanceof Integer &&
+                releaseYear.toString().length() == 4 &&
+                releaseYear.intValue() > 1000 && releaseYear.intValue() < 9999 ) {
+                return true;
+            } else {
+                throw new NumberFormatException("Invalid input for Release Year. " +
+                        "Please enter valid value for year in format YYYY");
+            }
+        }
+        return false;
+    }
 }
