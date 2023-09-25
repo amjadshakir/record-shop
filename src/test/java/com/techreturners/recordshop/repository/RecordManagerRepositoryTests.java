ce894a8df6c6a83bd3dfdd06122ea3ac5d3b032e
Swapna V <vswapna3202@gmail.com>
2023-09-25 21:30:56 +0100

    feat: added test case for repo

--- src/test/java/com/techreturners/recordshop/repository/RecordManagerRepositoryTests.java
+++ src/test/java/com/techreturners/recordshop/repository/RecordManagerRepositoryTests.java
@@ -1,7 +1,28 @@
 package com.techreturners.recordshop.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.techreturners.recordshop.model.MusicGenre;
+import com.techreturners.recordshop.model.MusicRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class RecordManagerRepositoryTests {
+
+    @Autowired
+    private RecordManagerRepository recordManagerRepository;
+
+    @Test
+    public void testCreatesAndFindBookByIdReturnsBook() {
+        MusicRecord musicRecord =
+                new MusicRecord(101L, "Album One nought One",
+                        "Artist One nought One", 2000,10L, MusicGenre.Metal);
+        recordManagerRepository.save(musicRecord);
+
+        var musicRecordId = recordManagerRepository.findById(musicRecord.getId());
+        assertThat(musicRecordId).isNotNull();
+    }
 
 }
