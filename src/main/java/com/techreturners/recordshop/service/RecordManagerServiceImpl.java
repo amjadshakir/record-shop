45dc64d7db564a0978e84ebc012b2086911f0428
Swapna V <vswapna3202@gmail.com>
2023-09-25 17:55:26 +0100

    feat: Added exception and Validator code and class

--- src/main/java/com/techreturners/recordshop/service/RecordManagerServiceImpl.java
+++ src/main/java/com/techreturners/recordshop/service/RecordManagerServiceImpl.java
@@ -1,8 +1,10 @@
 package com.techreturners.recordshop.service;
 
+import com.techreturners.recordshop.exception.InvalidRecordInputException;
 import com.techreturners.recordshop.exception.RecordAlreadyExistsException;
 import com.techreturners.recordshop.model.MusicRecord;
 import com.techreturners.recordshop.repository.RecordManagerRepository;
+import com.techreturners.recordshop.validator.MusicRecordValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,7 @@ public class RecordManagerServiceImpl implements RecordManagerService {
     @Override
     public MusicRecord insertMusicRecord(MusicRecord musicRecord) {
         String albumName = musicRecord.getAlbumName();
+
         if (albumName != null) {
             Optional<MusicRecord> bookOptional =
                     musicRecordManagerRepository.findByAlbumName(albumName);
@@ -25,43 +28,13 @@ public class RecordManagerServiceImpl implements RecordManagerService {
                         musicRecord.getAlbumName() +
                         " already exists and cannot be inserted again");
             }
-            validateInputValues(musicRecord);
         }
-        return musicRecordManagerRepository.save(musicRecord);
-    }
-
-    private boolean validateInputValues(MusicRecord musicRecord){
-        Integer releaseYear = musicRecord.getReleaseYear();
-        Long stock = musicRecord.getStock();
-        if (validateReleaseYear(releaseYear) && validateStock(stock))
-            return true;
-        return false;
-    }
-
-    private static boolean validateStock(Long stock) {
-        if (stock != null){
-            if (stock instanceof Long &&
-                stock >= 0) {
-                return true;
-            }else{
-                throw new NumberFormatException("Invalid input for Stock. "+
-                        "Please enter valid positive integer value for stock");
-            }
-        }
-        return false;
-    }
-
-    private static boolean validateReleaseYear(Integer releaseYear) {
-        if (releaseYear != null) {
-            if (releaseYear instanceof Integer &&
-                releaseYear.toString().length() == 4 &&
-                releaseYear.intValue() > 1000 && releaseYear.intValue() < 9999 ) {
-                return true;
-            } else {
-                throw new NumberFormatException("Invalid input for Release Year. " +
-                        "Please enter valid value for year in format YYYY");
-            }
-        }
-        return false;
+        boolean isValidReleaseYear = MusicRecordValidator.validateReleaseYear(musicRecord.getReleaseYear());
+        boolean isValidStock = MusicRecordValidator.validateStock(musicRecord.getStock());
+        if (isValidReleaseYear && isValidStock)
+            return musicRecordManagerRepository.save(musicRecord);
+        else
+            throw new InvalidRecordInputException("Invalid input entered. Please enter" +
+                    "only integers");
     }
 }
